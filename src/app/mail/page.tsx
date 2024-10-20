39fa07d12f73c20f63c756944b3b7e6511537f20
Arkashersz <extensed.wc@hotmail.com>
2024-10-20 17:58:48 -0300

    feat: Dark mode

--- src/app/mail/page.tsx
+++ src/app/mail/page.tsx
@@ -1,16 +1,25 @@
+import ThemeToggle from '@/components/theme-toggle'
 import dynamic from 'next/dynamic'
 import React from 'react'
 // import Mail from './mail'
 const Mail = dynamic(() => {
-  return import ('./mail')
+  return import('./mail')
 }, {
   ssr: false
 })
 
 const MailDashBoard = () => {
   return (
-    <Mail
-      defaultLayout={[20, 32, 48]} navCollapsedSize={0} defaultCollapsed={false} />
+    <>
+      <div className="absolute bottom-4 left-4">
+        <ThemeToggle />
+      </div>
+      <Mail
+        defaultLayout={[20, 32, 48]}
+        navCollapsedSize={0}
+        defaultCollapsed={false} />
+
+    </>
   )
 }
 
